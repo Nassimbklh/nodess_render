1f719384071ae749dbcd8a23f69da13d4ca3778c
Nassim Barkallah <75304726+Nassimbklh@users.noreply.github.com>
2024-02-08 09:39:58 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  process.env.VarStan, // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
